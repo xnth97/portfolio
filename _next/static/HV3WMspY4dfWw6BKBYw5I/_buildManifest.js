59cbd3a3dafd36c009dd126d70dff72f18633ac0
xnth97 <xnth97@live.com>
2024-03-09 07:54:57 +0000

    Update from https://github.com/xnth97/portfolio_src/commit/96d6b76c1f4d064aedecb4687364775408ff622a

---
+++ _next/static/HV3WMspY4dfWw6BKBYw5I/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/457-78f894b5fd54dbab.js","static/chunks/pages/index-abed4dfed9787ff4.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
